dc879a1316909dc43d1da6ac408462122fdfe574
Oliver_Juhasz <oliver_juhasz@epam.com>
2020-03-25 16:04:43 +0100

    Add witcher races as enum

--- domain/src/main/java/familiar/domain/character/witcher/wRace.java
+++ domain/src/main/java/familiar/domain/character/witcher/wRace.java
@@ -1,4 +1,15 @@
 package familiar.domain.character.witcher;
 
+import java.util.Collections;
+import java.util.Map;
+
 public enum wRace {
+    witcher(Map.of(wAbilities.Empathy, -4, wAbilities.Reflexes, 1), Map.of(wSkills.Awareness, 1)),
+    human(Map.of(wAbilities.Empathy, -4), Map.of(wSkills.Deduction, 1)),
+    dwarf(Map.of(), Map.of(wSkills.Physique, 1, wSkills.Business, 1)),
+    elf(Map.of(), Map.of(wSkills.FineArts, 1, wSkills.Archery, 2));
+
+
+    wRace(Map<wAbilities, Integer> abilities, Map<wSkills, Integer> skills) {
+    }
 }
