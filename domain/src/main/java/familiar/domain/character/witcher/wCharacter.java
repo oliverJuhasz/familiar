b7db84292bd52ff21ad461301eb5e1fd1fe47f19
Oliver_Juhasz <oliver_juhasz@epam.com>
2020-03-27 17:46:47 +0100

    Add witcher character class

--- domain/src/main/java/familiar/domain/character/witcher/wCharacter.java
+++ domain/src/main/java/familiar/domain/character/witcher/wCharacter.java
@@ -1,4 +1,13 @@
 package familiar.domain.character.witcher;
 
+import familiar.domain.character.witcher.profession.wProfession;
+
+import java.util.Map;
+
 public class wCharacter {
+    private Map<wAbilities, Integer> coreAbilities;
+    private Map<wSkills, Integer> coreSkills;
+    private wRace race;
+    private wProfession profession;
+
 }
