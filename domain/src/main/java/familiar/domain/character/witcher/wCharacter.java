0337fa37fdab44cff74035702cc0ec8058e50d84
Oliver_Juhasz <oliver_juhasz@epam.com>
2020-03-28 13:57:19 +0100

    Add builder to wCharacter

--- domain/src/main/java/familiar/domain/character/witcher/wCharacter.java
+++ domain/src/main/java/familiar/domain/character/witcher/wCharacter.java
@@ -1,13 +1,28 @@
 package familiar.domain.character.witcher;
 
+import familiar.domain.character.Gender;
+import familiar.domain.character.Name;
+import familiar.domain.character.RPGCharacter;
 import familiar.domain.character.witcher.profession.wProfession;
+import lombok.Builder;
+import lombok.ToString;
 
 import java.util.Map;
 
-public class wCharacter {
+@ToString
+public class wCharacter extends RPGCharacter {
     private Map<wAbilities, Integer> coreAbilities;
     private Map<wSkills, Integer> coreSkills;
     private wRace race;
     private wProfession profession;
 
+    @Builder
+    public wCharacter(Name name, Gender gender, int age, Map<wAbilities, Integer> coreAbilities, Map<wSkills, Integer> coreSkills,
+                      wRace race, wProfession profession) {
+        super(name, gender, age);
+        this.coreAbilities = coreAbilities;
+        this.coreSkills = coreSkills;
+        this.race = race;
+        this.profession = profession;
+    }
 }
