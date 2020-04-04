3f65e07a55aacf63f027f93b256b0b826271652f
Oliver_Juhasz <oliver_juhasz@epam.com>
2020-04-05 00:45:34 +0200

    Add PhysicalTable and HandToHandTable to character

--- domain/src/main/java/familiar/domain/character/witcher/wCharacter.java
+++ domain/src/main/java/familiar/domain/character/witcher/wCharacter.java
@@ -3,6 +3,8 @@ package familiar.domain.character.witcher;
 import familiar.domain.character.Gender;
 import familiar.domain.character.Name;
 import familiar.domain.character.RPGCharacter;
+import familiar.domain.character.witcher.assist.HandToHandTable;
+import familiar.domain.character.witcher.assist.PhysicalTable;
 import familiar.domain.character.witcher.profession.wProfession;
 import familiar.domain.character.witcher.profession.wProfessionSkills;
 import lombok.Builder;
@@ -17,6 +19,8 @@ public class wCharacter extends RPGCharacter {
     private Map<wProfessionSkills, Integer> professionSkills;
     private wRace race;
     private wProfession profession;
+    private PhysicalTable physicalTable = new PhysicalTable();
+    private HandToHandTable handToHandTable = new HandToHandTable();
 
     @Builder
     public wCharacter(Name name, Gender gender, int age, Map<wStats, Integer> coreAbilities, Map<wSkills, Integer> coreSkills,
