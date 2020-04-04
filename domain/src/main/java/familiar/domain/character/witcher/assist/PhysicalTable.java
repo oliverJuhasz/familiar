f7eae53bf34b5c9be0b6b71ef2685b07718916de
Oliver_Juhasz <oliver_juhasz@epam.com>
2020-04-04 17:06:45 +0200

    Implement PhysicalTable class

---
+++ domain/src/main/java/familiar/domain/character/witcher/assist/PhysicalTable.java
@@ -0,0 +1,46 @@
+package familiar.domain.character.witcher.assist;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhysicalTable {
+
+    private final Map<Integer, List<Integer>> physicalTableScores = new HashMap<>();
+
+    public PhysicalTable() throws IOException {
+        fillValuesFromFile();
+    }
+
+    private void fillValuesFromFile() throws IOException {
+        CsvReader reader = new CsvReader();
+        List<String[]> rawScores;
+        rawScores = reader.getData("PhysicalTable");
+        for (String[] line : rawScores) {
+            physicalTableScores.put(Integer.valueOf(line[0]),
+                    List.of(Integer.valueOf(line[1]), Integer.valueOf(line[1]),
+                            Integer.valueOf(line[1]), Integer.valueOf(line[1])));
+        }
+    }
+
+    public int bodyPlusWillRoundedDown(int bodyStat, int willStat) {
+        return (bodyStat + willStat) /2;
+    }
+
+    public int getHealthPoint(int bodyStat, int willStat) {
+        return physicalTableScores.get(bodyPlusWillRoundedDown(bodyStat, willStat)).get(1);
+    }
+
+    public int getStamina(int bodyStat, int willStat) {
+        return physicalTableScores.get(bodyPlusWillRoundedDown(bodyStat, willStat)).get(2);
+    }
+
+    public int getRecovery(int bodyStat, int willStat) {
+        return physicalTableScores.get(bodyPlusWillRoundedDown(bodyStat, willStat)).get(3);
+    }
+
+    public int getStun(int bodyStat, int willStat) {
+        return physicalTableScores.get(bodyPlusWillRoundedDown(bodyStat, willStat)).get(4);
+    }
+}
