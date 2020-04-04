6f421e4cc2151d26974fac8425346d8fac47584e
Oliver_Juhasz <oliver_juhasz@epam.com>
2020-04-04 17:07:06 +0200

    Implement CsvReader class

---
+++ domain/src/main/java/familiar/domain/character/witcher/assist/CsvReader.java
@@ -0,0 +1,28 @@
+package familiar.domain.character.witcher.assist;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CsvReader {
+
+    private String separator = ","; //TODO - as Bean
+    private String line;
+
+    public List<String[]> getData(String resourceName) throws IOException {
+        List<String[]> result = new ArrayList<>();
+        File file = new File(
+                Objects.requireNonNull(getClass().getClassLoader().getResource(resourceName)).getFile());
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        while ((line = br.readLine()) != null) {
+            String[] scores = line.split(separator);
+            result.add(scores);
+        }
+        return result;
+    }
+
+}
