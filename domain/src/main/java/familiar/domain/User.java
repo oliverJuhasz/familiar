d503ac9df9f6d2bb5c239916c856a639c3203987
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-16 15:24:01 +0200

    Add builders

--- domain/src/main/java/familiar/domain/User.java
+++ domain/src/main/java/familiar/domain/User.java
@@ -1,12 +1,14 @@
 package familiar.domain;
 
 import familiar.domain.character.RPGCharacter;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 public class User {
 
     private long id;
