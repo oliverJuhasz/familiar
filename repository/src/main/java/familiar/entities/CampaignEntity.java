1f820a9bfee1f91d3d63ac3a7e91afee94bd9e87
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-17 11:15:03 +0200

    Add backtick to SQl naming to make H2 case sensitive

--- repository/src/main/java/familiar/entities/CampaignEntity.java
+++ repository/src/main/java/familiar/entities/CampaignEntity.java
@@ -12,34 +12,34 @@ import java.util.List;
 @Data
 @Entity
 @NoArgsConstructor
-@Table(name = "Campaigns")
+@Table(name = "`Campaigns`")
 public class CampaignEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CampaignID")
+    @Column(name = "`CampaignID`")
     private Long id;
 
-    @Column(name = "Name")
+    @Column(name = "`Name`")
     private String name;
 
-    @Column(name = "World")
+    @Column(name = "`World`")
     private World world;
 
     @ManyToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(name = "player_campaign",
+    @JoinTable(name = "`player_campaign`",
                 joinColumns = @JoinColumn(name = "campaign_id"),
                 inverseJoinColumns = @JoinColumn(name = "player_id"))
     private List<PlayerEntity> players;
 
-    @Column(name = "Created")
+    @Column(name = "`Created`")
     private LocalDateTime created;
 
     @OneToMany(mappedBy="campaign", cascade = CascadeType.PERSIST)
     private List<SessionEntity> sessions;
 
     @OneToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(name = "CampaignNote",
+    @JoinTable(name = "`CampaignNote`",
                 joinColumns = @JoinColumn(name = "CampaignID"),
                 inverseJoinColumns = @JoinColumn(name = "NoteId"))
     private List<NoteEntity> notes;
