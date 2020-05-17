d1f49a3132f377ef0f2d09699639dcbdc40c3eb7
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-17 17:29:32 +0200

    Map WitcherCharacterEntity

--- repository/src/main/java/familiar/entities/character/witcher/wCharacterEntity.java
+++ repository/src/main/java/familiar/entities/character/witcher/wCharacterEntity.java
@@ -17,27 +17,31 @@ public class wCharacterEntity extends RPGCharacterEntity {
 
     @ElementCollection(fetch = FetchType.LAZY)
     @CollectionTable(name = "`WitcherCharacter_CoreAbilities`", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "xyz"), joinColumns = @JoinColumn(name = "`CoreAbilityID`"))
-    @MapKeyColumn(name = "`Ability`", length = 40, nullable = false)
+    @MapKeyColumn(name = "`Ability`", length = 20)
     @MapKeyEnumerated(EnumType.STRING)
     @Column(name = "`CoreAbilityValue`")
     private Map<wStats, Integer> coreAbilities;
 
     @ElementCollection(fetch = FetchType.LAZY)
     @CollectionTable(name = "`WitcherCharacter_CoreSkills`", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "xyz2"), joinColumns = @JoinColumn(name = "`CoreSkillID`"))
-    @MapKeyColumn(name = "`Skill`", length = 40, nullable = false)
+    @MapKeyColumn(name = "`Skill`", length = 20)
     @MapKeyEnumerated(EnumType.STRING)
     @Column(name = "`CoreSkillValue`")
     private Map<wSkills, Integer> coreSkills;
 
     @ElementCollection(fetch = FetchType.LAZY)
     @CollectionTable(name = "`WitcherCharacter_ProfessionSkills`", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "xyz3"), joinColumns = @JoinColumn(name = "`ProfessionID`"))
-    @MapKeyColumn(name = "`Profession`", length = 40, nullable = false)
+    @MapKeyColumn(name = "`Profession`", length = 40)
     @MapKeyEnumerated(EnumType.STRING)
     @Column(name = "`CoreSkillValue`")
     private Map<wProfessionSkills, Integer> professionSkills;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "`Race`", nullable = false, length = 20)
     private wRace race;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "`Profession`", nullable = false, length = 20)
     private wProfession profession;
 
 }
