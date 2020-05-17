944dc55ead98b5d12236146560ae315caff566fe
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-17 15:19:12 +0200

    Change naming for NoteEntity

--- repository/src/main/java/familiar/entities/NoteEntity.java
+++ repository/src/main/java/familiar/entities/NoteEntity.java
@@ -2,22 +2,26 @@ package familiar.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name = "`Notes`")
 public class NoteEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "`NoteID`")
     private long id;
 
+    @Column(name = "`Title`")
     private String title;
+
+    @Column(name = "`Text`")
     private String text;
+
+    @Column(name = "`Created`")
     private LocalDateTime created;
 
 }
