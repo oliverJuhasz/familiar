426b1b1e58cdd485f85f6723ef80b612395ac233
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-17 10:36:58 +0200

    Add proper naming convention to CampaignEntity

--- repository/src/main/java/familiar/entities/CampaignEntity.java
+++ repository/src/main/java/familiar/entities/CampaignEntity.java
@@ -12,12 +12,18 @@ import java.util.List;
 @Data
 @Entity
 @NoArgsConstructor
+@Table(name = "Campaigns")
 public class CampaignEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CampaignID")
     private Long id;
+
+    @Column(name = "Name")
     private String name;
+
+    @Column(name = "World")
     private World world;
 
     @ManyToMany(cascade = CascadeType.PERSIST)
@@ -26,14 +32,15 @@ public class CampaignEntity {
                 inverseJoinColumns = @JoinColumn(name = "player_id"))
     private List<PlayerEntity> players;
 
+    @Column(name = "Created")
     private LocalDateTime created;
 
     @OneToMany(mappedBy="campaign", cascade = CascadeType.PERSIST)
     private List<SessionEntity> sessions;
 
     @OneToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(name = "campaign_note",
-                joinColumns = @JoinColumn(name = "campaign_id"),
-                inverseJoinColumns = @JoinColumn(name = "note_id"))
+    @JoinTable(name = "CampaignNote",
+                joinColumns = @JoinColumn(name = "CampaignID"),
+                inverseJoinColumns = @JoinColumn(name = "NoteId"))
     private List<NoteEntity> notes;
 }
