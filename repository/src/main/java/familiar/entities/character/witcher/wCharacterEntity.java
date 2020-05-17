674b6ffc895e9291d0518e9f3f8ad93c9366039f
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-17 16:53:58 +0200

    Map wCharacterEntity

--- repository/src/main/java/familiar/entities/character/witcher/wCharacterEntity.java
+++ repository/src/main/java/familiar/entities/character/witcher/wCharacterEntity.java
@@ -1,21 +1,43 @@
 package familiar.entities.character.witcher;
 
 import familiar.entities.character.RPGCharacterEntity;
+import familiar.entities.character.witcher.profession.wProfession;
+import familiar.entities.character.witcher.profession.wProfessionSkills;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Map;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
-@Table(name = "WitcherCharacters")
+@Table(name = "`WitcherCharacters`")
 public class wCharacterEntity extends RPGCharacterEntity {
-//    private Map<wStats, Integer> coreAbilities;
-//    private Map<wSkills, Integer> coreSkills;
-//    private Map<wProfessionSkills, Integer> professionSkills;
-//    private wRace race;
-//    private wProfession profession;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "`WitcherCharacter_CoreAbilities`", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "xyz"), joinColumns = @JoinColumn(name = "`CoreAbilityID`"))
+    @MapKeyColumn(name = "`Ability`", length = 40, nullable = false)
+    @MapKeyEnumerated(EnumType.STRING)
+    @Column(name = "`CoreAbilityValue`")
+    private Map<wStats, Integer> coreAbilities;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "`WitcherCharacter_CoreSkills`", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "xyz2"), joinColumns = @JoinColumn(name = "`CoreSkillID`"))
+    @MapKeyColumn(name = "`Skill`", length = 40, nullable = false)
+    @MapKeyEnumerated(EnumType.STRING)
+    @Column(name = "`CoreSkillValue`")
+    private Map<wSkills, Integer> coreSkills;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "`WitcherCharacter_ProfessionSkills`", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "xyz3"), joinColumns = @JoinColumn(name = "`ProfessionID`"))
+    @MapKeyColumn(name = "`Profession`", length = 40, nullable = false)
+    @MapKeyEnumerated(EnumType.STRING)
+    @Column(name = "`CoreSkillValue`")
+    private Map<wProfessionSkills, Integer> professionSkills;
+
+    private wRace race;
+
+    private wProfession profession;
 
 }
