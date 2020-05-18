493fa01979861bab170907b8854764e6a3f53a40
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-18 17:59:25 +0200

    Remove session and character info from player

--- repository/src/main/java/familiar/entities/PlayerEntity.java
+++ repository/src/main/java/familiar/entities/PlayerEntity.java
@@ -1,11 +1,16 @@
 package familiar.entities;
 
 import familiar.entities.character.RPGCharacterEntity;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -16,12 +21,15 @@ import java.util.List;
 @Table(name = "`Players`")
 public class PlayerEntity extends UserEntity {
 
+    @Builder
+    public PlayerEntity(long id, String userName, String password, LocalDateTime created, String name,
+                        List<CampaignEntity> campaigns, List<RPGCharacterEntity> characters) {
+        super(id, userName, password, created);
+        this.name = name;
+        this.campaign = campaigns;
+        this.characters = characters;
+    }
+
     @Column(name = "`Name`")
     private String name;
-
-    @ManyToMany(mappedBy="players")
-    private List<CampaignEntity> campaign;
-
-    @OneToMany(mappedBy="owner")
-    private List<RPGCharacterEntity> characters;
 }
--- service/src/main/java/familiar/service/user/domain/Player.java
+++ service/src/main/java/familiar/service/user/domain/Player.java
@@ -1,19 +1,13 @@
 package familiar.service.user.domain;
 
 import familiar.entities.UserEntity;
-import familiar.service.campaign.domain.Campaign;
-import familiar.service.character.domain.RpgCharacter;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Player extends UserEntity {
 
     private String name;
-    private List<Campaign> campaign;
-    private List<RpgCharacter> characters;
 
 }
