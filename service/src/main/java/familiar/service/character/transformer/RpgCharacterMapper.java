fc8e3c060b3db1fd25f1a6312c522cb54713abf0
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-14 22:39:38 +0200

    Add support converters

--- service/src/main/java/familiar/service/character/transformer/RpgCharacterMapper.java
+++ service/src/main/java/familiar/service/character/transformer/RpgCharacterMapper.java
@@ -3,20 +3,25 @@ package familiar.service.character.transformer;
 import familiar.entities.character.RpgCharacterEntity;
 import familiar.entities.character.witcher.WitcherCharacterEntity;
 import familiar.service.campaign.transformer.CycleAvoidingMappingContext;
+import familiar.service.character.domain.Gender;
 import familiar.service.character.domain.RpgCharacter;
+import familiar.service.character.domain.Status;
 import familiar.service.character.domain.witcher.WitcherCharacter;
+import familiar.service.character.domain.witcher.WitcherRace;
+import familiar.service.character.domain.witcher.WitcherSkills;
+import familiar.service.character.domain.witcher.WitcherStats;
+import familiar.service.character.domain.witcher.profession.WitcherProfession;
+import familiar.service.character.domain.witcher.profession.WitcherProfessionSkills;
 import familiar.service.note.transformer.NoteMapper;
 import familiar.service.user.transformer.UserMapper;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Service
 @Mapper(componentModel = "spring")
 public abstract class RpgCharacterMapper {
 
@@ -62,10 +67,67 @@ public abstract class RpgCharacterMapper {
         return characterEntity;
     }
 
+    public RpgCharacter rpgCharacterEntityToRgpCharacter(RpgCharacterEntity rpgCharacterEntity, @Context CycleAvoidingMappingContext context) {
+        RpgCharacter target = context.getMappedInstance(rpgCharacterEntity, RpgCharacter.class);
+        if (target != null) {
+            return target;
+        }
+
+        if (rpgCharacterEntity == null) {
+            return null;
+        }
+
+        WitcherCharacterEntity from = (WitcherCharacterEntity) rpgCharacterEntity;
+        RpgCharacter character = WitcherCharacter.builder()
+                .age(from.getAge())
+                .coreAbilities(convertAbilityToKey(from.getCoreAbilities()))
+                .coreSkills(convertCoreSkillToKey(from.getCoreSkills()))
+                .created(from.getCreated())
+                .gender(Gender.valueOf(from.getGender()))
+                .id(from.getId())
+                .name(nameMapper.convertNameEntityToName(from.getNameEntity(), context))
+                .owner(userMapper.userEntityToUser(from.getOwner(), context))
+                .profession(WitcherProfession.valueOf(from.getProfession()))
+                .professionSkills(convertProfessionSkillToKey(from.getProfessionSkills()))
+                .race(WitcherRace.valueOf(from.getRace()))
+                .status(Status.valueOf(from.getStatus()))
+                .storyTellerNotes(rpgCharacterEntity.getStoryTellerNotes()
+                        .stream()
+                        .map(noteEntity -> noteMapper.noteEntityToNote(noteEntity, context))
+                        .collect(Collectors.toList()))
+                .build();
+        context.storeMappedInstance(rpgCharacterEntity, character);
+        return character;
+    }
+
     private Map<String, Integer> convertKeyToString(Map<?, Integer> abilities) {
         Map<String, Integer> converted = new HashMap<>();
         for (Map.Entry<?, Integer> ability : abilities.entrySet()) {
             converted.put(ability.getKey().toString(), ability.getValue());
+        }
+        return converted;
+    }
+
+    private Map<WitcherStats, Integer> convertAbilityToKey(Map<String, Integer> abilities) {
+        Map<WitcherStats, Integer> converted = new HashMap<>();
+        for (Map.Entry<String, Integer> ability : abilities.entrySet()) {
+            converted.put(WitcherStats.valueOf(ability.getKey()), ability.getValue());
+        }
+        return converted;
+    }
+
+    private Map<WitcherSkills, Integer> convertCoreSkillToKey(Map<String, Integer> abilities) {
+        Map<WitcherSkills, Integer> converted = new HashMap<>();
+        for (Map.Entry<String, Integer> ability : abilities.entrySet()) {
+            converted.put(WitcherSkills.valueOf(ability.getKey()), ability.getValue());
+        }
+        return converted;
+    }
+
+    private Map<WitcherProfessionSkills, Integer> convertProfessionSkillToKey(Map<String, Integer> abilities) {
+        Map<WitcherProfessionSkills, Integer> converted = new HashMap<>();
+        for (Map.Entry<String, Integer> ability : abilities.entrySet()) {
+            converted.put(WitcherProfessionSkills.valueOf(ability.getKey()), ability.getValue());
         }
         return converted;
     }
