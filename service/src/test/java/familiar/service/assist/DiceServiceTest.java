23626b07ad1c91431d40ab905bd066f163d117ea
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-06-28 23:17:42 +0200

    Add test to evaluate complex diceExpression

--- service/src/test/java/familiar/service/assist/DiceServiceTest.java
+++ service/src/test/java/familiar/service/assist/DiceServiceTest.java
@@ -2,11 +2,13 @@ package familiar.service.assist;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DiceServiceTest {
@@ -24,6 +26,19 @@ public class DiceServiceTest {
     @DisplayName("Invalid expressions should result in error.")
     public void test1(String input) {
         assertThrows(IllegalArgumentException.class, () -> underTest.evaluateDiceExpression(input));
+    }
+
+    @Test
+    @DisplayName("DiceService calculates 15d1-3+5d1+2 expression properly")
+    public void test2() {
+        // GIVEN
+        String diceExpression = "15d1-3+5d1+2";
+
+        // WHEN
+        int result = underTest.evaluateDiceExpression(diceExpression);
+
+        // THEN
+        assertEquals(19, result);
     }
 
 }
