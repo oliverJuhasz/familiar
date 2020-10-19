e1c8bc7c0ce437989ba3eafe11c7c4b2503fd798
Oliver Juhasz <oliverj@topdesk.com>
2020-10-19 15:38:06 +0200

    Enable additional dependencies

--- service/src/main/java/familiar/App.java
+++ service/src/main/java/familiar/App.java
@@ -1,6 +1,8 @@
 package familiar;
 
+import familiar.service.assist.DiceService;
 import familiar.service.campaign.service.TestDataService;
+import familiar.service.character.service.WitcherCharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,12 @@ public class App implements CommandLineRunner {
     @Autowired
     private TestDataService testDataService;
 
+    @Autowired
+    private DiceService diceService;
+
+    @Autowired
+    private WitcherCharacterService witcherCharacterService;
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
@@ -21,5 +29,7 @@ public class App implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         testDataService.addTestData();
+        System.out.println(diceService.evaluateDiceExpression("2d6"));
+        System.out.println(witcherCharacterService.findWitcherCharacterById(1));
     }
 }
