7974e61654f7d9438c7fffb94de4728e7fdaffb1
Oliver Juhasz <oliverj@topdesk.com>
2021-01-25 18:26:07 +0100

    Add web module with test controller

---
+++ web/src/main/java/familiar/api/character/CharacterController.java
@@ -0,0 +1,21 @@
+package familiar.api.character;
+
+import familiar.service.character.domain.witcher.WitcherCharacter;
+import familiar.service.character.service.WitcherCharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CharacterController {
+
+    @Autowired
+    WitcherCharacterService witcherCharacterService;
+
+    @GetMapping("/characters")
+    public WitcherCharacter getCharacters() {
+        return witcherCharacterService.findWitcherCharacterById(1);
+    }
+}
